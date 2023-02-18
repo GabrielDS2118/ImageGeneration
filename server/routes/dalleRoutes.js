b4bc457dcfaf0867dd2238c40730c42bc1d20bd1
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-02-18 11:19:01 -0500

    8.Configuration OpenAI

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -5,4 +5,13 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const router = express.Router();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+  res.send('Hello from DALLE-E!');
+});
 export default router;
